315356484a0461e9c4041f0afc8f51154aca843e
Burak Yılmaz <burakyilmaz2908@gmail.com>
2026-02-24 17:10:15 +0300

    Trigger deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://yourportfolio.dev',
+  site: 'https://brksfrb.com',
   integrations: [sitemap()],
 });
